6e4aa4dca9bde05409246eb855b75c3b25a7f39a
DininduChamikara <dininduchamikara99@gmail.com>
2025-07-06 20:19:46 +0530

    added: sample route

---
+++ src/components/CdStepper.vue
@@ -0,0 +1,30 @@
+<!-- eslint-disable vue/valid-v-slot -->
+<template>
+  <v-stepper prev-text="Previous" next-text="Next" :items="['Step 1', 'Step 2', 'Step 3']">
+    <template v-slot:item.1>
+      <v-card title="Step One" flat
+        >Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti consequatur repellat
+        aliquam, culpa quisquam saepe maiores natus aspernatur dolorum, nam dignissimos inventore
+        molestias ut, doloribus sint recusandae eos autem incidunt.</v-card
+      >
+    </template>
+
+    <template v-slot:item.2>
+      <v-card title="Step Two" flat
+        >Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti consequatur repellat
+        aliquam, culpa quisquam saepe maiores natus aspernatur dolorum, nam dignissimos inventore
+        molestias ut, doloribus sint recusandae eos autem incidunt.</v-card
+      >
+    </template>
+
+    <template v-slot:item.3>
+      <v-card title="Step Three" flat
+        >Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti consequatur repellat
+        aliquam, culpa quisquam saepe maiores natus aspernatur dolorum, nam dignissimos inventore
+        molestias ut, doloribus sint recusandae eos autem incidunt.</v-card
+      >
+    </template>
+  </v-stepper>
+</template>
+
+<script setup lang="ts"></script>
---
+++ src/views/SampleView.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>Sample Page</h1>
+  <cd-stepper></cd-stepper>
+</template>
+
+<script lang="ts">
+import CdStepper from '@/components/CdStepper.vue'
+
+export default {
+  components: {
+    'cd-stepper': CdStepper,
+  },
+}
+</script>
