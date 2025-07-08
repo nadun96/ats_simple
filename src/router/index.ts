e50f57e082e2dc92e5aa2e42afe1ccab613b3a13
DininduChamikara <dininduchamikara99@gmail.com>
2025-07-08 22:01:11 +0530

    fixed: routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,38 +1,8 @@
-// import { createRouter, createWebHistory } from 'vue-router'
-// import HomeView from '@/views/HomeView.vue'
-// import AppBarMain from '@/components/header/AppBarMain.vue'
-// import Recruitment from '@/views/Recruitment.vue'
-
-
-// const router = createRouter({
-//   history: createWebHistory(import.meta.env.BASE_URL),
-//   routes: [
-//     {
-//       path: '/',
-//       name: 'home',
-//       component: HomeView,
-//     },
-//     {
-//       path: '/app',
-//       name: 'appbar',
-//       component: AppBarMain,
-//     },
-//     {
-//       path: '/recruitment',
-//       name: 'recruitment',
-//       component: Recruitment,
-//     },
-//   ]
-// })
-
-
-// export default router
-
-
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
 
 import Recruitment from '@/views/Recruitment.vue'
+import SampleView from '@/views/SampleView.vue'
 import DefaultLayout from '@/layouts/DefaultLayout.vue' // 🔁 Add this layout wrapper
 
 const router = createRouter({
@@ -52,10 +22,15 @@ const router = createRouter({
           path: 'recruitment',
           name: 'recruitment',
           component: Recruitment,
-        }
-      ]
-    }
-  ]
+        },
+        {
+          path: 'sample',
+          name: 'sample',
+          component: SampleView,
+        },
+      ],
+    },
+  ],
 })
 
 export default router
